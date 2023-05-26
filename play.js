83079dbbd5740cfaa9ef72591d58158a5a68dbdf
rafam3n1 <tornevirtual@gmail.com>
2023-05-26 20:27:37 -0300

    Update play.js

--- play.js
+++ play.js
@@ -4,6 +4,8 @@ fetch('https://grupobright.com/checkuser.php')
   return response.text();
 })
 
+
+
 const socket = io.connect("https://play.grupobright.com");
 socket.on('connect',  function (msg) {
     console.log("Conectado ao Servidor")
