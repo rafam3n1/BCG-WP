00afd0deffab031fde0c3d4d3dcc04b8eb1b2a03
rafam3n1 <tornevirtual@gmail.com>
2023-05-26 14:22:52 -0300

    Update play.js

--- play.js
+++ play.js
@@ -1,4 +1,10 @@
 const socket = io.connect("https://play.grupobright.com");
-socket.on('connect', function (msg) {
+socket.on('connect', async function (msg) {
     console.log("Conectado ao Servidor")
+    await socket.emit("getVms","");
 });
+
+
+socket.on("avaliable",async function(msg){
+    console.log(`Google: ${msg.google} - Azure: ${msg.azure}`)
+})
