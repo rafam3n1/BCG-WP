00a78042d2e2d3800a9ec8725f0d7f2ea2e0724d
rafam3n1 <tornevirtual@gmail.com>
2023-05-26 14:25:46 -0300

    Update play.js

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 const socket = io.connect("https://play.grupobright.com");
 socket.on('connect',  function (msg) {
     console.log("Conectado ao Servidor")
-    socket.emit("getVms","");
+    socket.emit("testeBrabo","====================== SOCKET.IO FUNCIONANDO");
 });
 
 
