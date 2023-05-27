eb96d7af7a26c3edc879072a6385115d789e799e
rafam3n1 <tornevirtual@gmail.com>
2023-05-26 21:00:03 -0300

    Update play.js

--- play.js
+++ play.js
@@ -10,7 +10,7 @@ const socket = io.connect("https://play.grupobright.com");
 
 socket.on('connect',  function (msg) {
     console.log("Conectado ao Servidor")
-    socket.emit('authenticate', { token: tokenSelecionado });
+    socket.emit('authenticate', tokenSelecionado);
 });
 
 
