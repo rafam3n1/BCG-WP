26e395459e3a358673dab8db335b9e8a03f69d03
jonzinho <67214224+jonzinho@users.noreply.github.com>
2025-11-09 19:37:34 -0300

    Update play.js

--- play.js
+++ play.js
@@ -323,7 +323,7 @@ $(document).ready(function () {
     $("#jogos")[0].style.display = "flex";
   };
 
-  // RESETAR VM
+  // RESETAR VM (botão na VM criada)
   $("#popupreset-botao")[0].onclick = function () {
     elementorProFrontend.modules.popup.showPopup({ id: 49488 });
     $("#resetar-botao")[0].onclick = function () {
@@ -333,9 +333,14 @@ $(document).ready(function () {
     };
   };
 
- // RESETAR VM (botão na tela de loading com confirmação)
+  // RESETAR VM (botão na tela de loading)
   $("#loading-reset")[0].onclick = function () {
     elementorProFrontend.modules.popup.showPopup({ id: 49488 });
+    $("#resetar-botao")[0].onclick = function () {
+      socket.emit("reset", "");
+      $("#loading")[0].style.display = "flex";
+      $("#vm-criada")[0].style.display = "none";
+    };
   };
 
   $("#interromper-botao")[0].onclick = function () {
